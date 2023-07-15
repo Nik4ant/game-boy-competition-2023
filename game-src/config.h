6667138ff75c01efcfb23084d3e1ccb21becface
Nikita <Nik4ant@yandex.ru>
2023-07-15 16:47:08 +0700

    build.py detects all .c files; vscode emulicious set up; tile desginer; almost working color bg tiles; bgb emulator

---
+++ game-src/config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+const static uint8_t SCREEN_SIZE[2] = {160, 144};
+
+#endif
--- game-src/main.c
+++ game-src/main.c
@@ -1,7 +1,34 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "config.h"
+#include "assets/TILE_EMPTY.h"
+#include "assets/TILES_NUMBERS.h"
+
 
 void main()
 {
-	printf("Hello world");
+	printf("Hello world!");
+	printf("Game boy resolution is: %dx%d", SCREEN_SIZE[0], SCREEN_SIZE[1]);
+
+	// set_bkg_data(0, 1, TILE_EMPTY);
+	set_bkg_data(0, 10, TILES_NUMBERS);
+	init_bkg(0);
+
+	// TODO: look at game examples in the gbdk repo to see how they render colors
+	SHOW_BKG;
+	DISPLAY_ON;
+
+	// Gameloop
+	while (true) {
+		uint8_t input = joypad();
+
+		// scroll_bkg(1, 1);
+
+		// delay(200);
+		// Done processing, yield CPU and wait for start of next frame
+		wait_vbl_done();
+	}
 }
